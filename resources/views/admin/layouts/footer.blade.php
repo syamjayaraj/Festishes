a7f3c75ec824110fd34e689f22d329c1bac77b73
Syamlal CM <syamjayaraj@gmail.com>
2017-11-14 22:11:02 +0530

    basic_launch_version_final

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,5 +1,7 @@
-<hr>
+<!-- Footer -->
 <footer>
-	<p>&copy; Festishes 2017</p>
+  <div class="container text-center">
+    <p>Made with Love in God's own country | Designed by <a href="http://devhornet.com">DevHornet</a></p>
+    <p>Copyright &copy; <a href="http://floyet.com">FloYET</a> 2017</p>
+  </div>
 </footer>
-</div><!--/.container-->
